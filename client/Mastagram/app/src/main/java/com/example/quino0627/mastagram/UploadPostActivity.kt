a9b48c9e9e7e3f180dbed26afa85d82e3a26adab
송동욱 <quino0627@korea.ac.kr>
2019-01-05 03:32:08 +0900

    client add

---
+++ client/Mastagram/app/src/main/java/com/example/quino0627/mastagram/UploadPostActivity.kt
@@ -0,0 +1,24 @@
+package com.example.quino0627.mastagram
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_upload_post.*
+
+class UploadPostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_post)
+
+        val Picture = intent.getStringExtra("image")
+        Log.d("THIS IS INTENTED IMAGE", Picture)
+        val picture_uri = Uri.parse(Picture)
+        //val imageView = findViewById<ImageView>(R.id.fullScreenImageView)
+
+        val imageView = uploading_image
+        imageView.setImageURI(picture_uri)
+        //imageView.setImageResource(imgResId)
+    }
+}
